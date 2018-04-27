28a9569473e063938aadabe9241c7d8ef3564815
Kareem <kareememad400@gmail.com>
2018-04-27 13:42:05 +0200

    angular 4 interface added

---
+++ assets/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+const template =  '<h1>My First Angular 2 App</h1>'
+
+
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'templates/app.component.html'
+})
+export class AppComponent { }
+
